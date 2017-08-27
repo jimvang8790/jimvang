2b1785b5c5df5406d6e97df257abd7701d2a6cb8
Jim Vang <vangjim15@gmail.com>
2017-08-27 14:36:41 -0500

    added info to project

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,16 +2,16 @@ var myApp = angular.module('myApp', ['ngRoute']);
 // Angular configuration (routes)
 myApp.config(function($routeProvider, $locationProvider) {
   $routeProvider.when('/', {
-    template: 'views/index.html'
+    template: ''
   }).when('/agentave', {
-    templateUrl: 'views/agentave.html',
-    controller: 'AgentController as ac'
+    templateUrl: 'views/projects/agentave.html',
+    // controller: 'AgentController as ac'
   }).when('/bouncetribe', {
     templateUrl: 'views/bouncetribe.html',
-    controller: 'BounceController as bc'
+    // controller: 'BounceController as bc'
   }).when('/stepup', {
     templateUrl: 'views/stepup.html',
-    controller: 'StepController as sc'
+    // controller: 'StepController as sc'
   }).otherwise('/');
   $locationProvider.html5Mode(true);
 });// end config
