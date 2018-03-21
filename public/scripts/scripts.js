f5e27ed22dc99da0efccc5f00b5a784e002b7245
Jim Vang <vangjim15@gmail.com>
2018-03-21 15:15:57 -0500

    add video and text to header

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -3,5 +3,16 @@ $(document).ready(function() {
         $(window).on('load resize', function() {
             $('.fill-screen').css('height', window.innerHeight);
         });
-    }); 
+    });
+    
+    $("#video-wallpaper").wallpaper({
+        source: {
+            poster: "../images/landing_bkg.png",
+            video: "//www.youtube.com/embed/zSUtUG3y3b4&index=63&list=PLdvyw4s_Y87UD9IIEPqbq47qEmd84QSLb&t=16s"
+            // webm: "http://example.com/video.webm",
+            // mp4: "../video/landing_bkg.mp4,
+            // ogg: "http://example.com/video.ogv"
+        }
+    }); // end video function
+
 })
