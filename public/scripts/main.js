00140868ef89abb9b2f631ae0251dc811a19596e
Jim Vang <vangjim15@gmail.com>
2018-03-24 17:20:49 -0500

    added parallax scrolling

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,18 +8,21 @@ $(document).ready(function() {
     // Scrollspy
     $('body').scrollspy({
         target: '.navbar',
-        offset: 160
+        offset: 160 // offsetting line 17
     }); // end scroolspy
 
-    // smooth scrolling jquery easing
+    // smooth scrolling jquery.easing
     $('.nav a, .down-button a, .overlay-banner a').bind('click', function () {
         $('html, body').stop().animate({
             scrollTop: $($(this).attr('href')).offset().top - 68
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     }); // end smooth scrolling
+
+    // parallax scrolling with stellar.js
+    $(window).stellar();
     
-    // Adding video from youtube/mp4 as header
+    // adding video from youtube/mp4 as header
     $("#video-wallpaper").wallpaper({
         source: {
             poster: "../images/landing_bkg.png",
