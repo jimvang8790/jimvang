1dd317617fb5169f3705d605a7a8a588059ee226
Jim Vang <vangjim15@gmail.com>
2018-03-24 16:47:36 -0500

    added smooth scolling

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,7 +4,22 @@ $(document).ready(function() {
             $('.fill-screen').css('height', window.innerHeight);
         });
     });
+
+    // Scrollspy
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 160
+    }); // end scroolspy
+
+    // smooth scrolling jquery easing
+    $('.nav a, .down-button a, .overlay-banner a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 68
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    }); // end smooth scrolling
     
+    // Adding video from youtube/mp4 as header
     $("#video-wallpaper").wallpaper({
         source: {
             poster: "../images/landing_bkg.png",
@@ -15,4 +30,4 @@ $(document).ready(function() {
         }
     }); // end video function
 
-})
+}); // end document.ready
