0d10a931ab5880553b7e37af4ba180781265281b
Jim Vang <vangjim15@gmail.com>
2018-03-24 20:36:29 -0500

    added mp4 video to header

--- public/js/main.js
+++ public/js/main.js
@@ -26,10 +26,7 @@ $(document).ready(function() {
     $("#video-wallpaper").wallpaper({
         source: {
             poster: "../images/landing_bkg.png",
-            video: "//www.youtube.com/embed/zSUtUG3y3b4&index=63&list=PLdvyw4s_Y87UD9IIEPqbq47qEmd84QSLb&t=16s"
-            // webm: "http://example.com/video.webm",
-            // mp4: "../video/landing_bkg.mp4,
-            // ogg: "http://example.com/video.ogv"
+            mp4: "../video/Plexus_AfterEffects.mp4"
         }
     }); // end video function
 
